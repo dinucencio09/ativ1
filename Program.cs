e7fbfcf1787ce18d9af36e18fbfe34c84253a48d
dinucencio09 <133662891+dinucencio09@users.noreply.github.com>
2024-05-29 10:32:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+        /*1 Exercício*/
+
+        /*int salarioPorHora = 35;
+        int horasTrabalhadas = 8;
+        int salarioDiario = salarioPorHora * horasTrabalhadas;
+        Console.WriteLine("O salário diário de Joaquim é: R$" + salarioDiario);*/
+
+        /*2 Exercício*/
+
+        /*int unidadesPorCaixa = 27;
+        int unidadesCompradas = 16;
+        int totalBombons = unidadesPorCaixa * unidadesCompradas;
+        Console.WriteLine("Pamela tem " + totalBombons + " bombons.");*/
+
+        /*3 Exercício*/
+
+        /*int inteiro2 = 5;
+        int inteiro3 = 10;
+        int soma = inteiro2 + inteiro3;
+        Console.WriteLine("Digite dois números inteiros separados por espaço: " + soma);*/
+
+        /*4 Exercício*/
+
+        /*int garrafasPorCaixa = 20;
+        int totalGarrafasVendidas = 300;
+        int totalCaixasUsadas = totalGarrafasVendidas / garrafasPorCaixa;
+        Console.WriteLine("Foram usadas " + totalCaixasUsadas + " caixas.");*/
+
+        /*Quinto Exercício*/
+
+        /*double precoInstalacao = 300.00;
+        double precoLimpeza = 150.00;
+        double precoManutencao = 499.99;
+        double descontoPromocional = 50.00;
+
+        double totalComDesconto = precoInstalacao + precoLimpeza + precoManutencao - descontoPromocional;
+
+        Console.WriteLine("O cliente deverá pagar à empresa: R$" + totalComDesconto);*/
+    }
+}
+
